f9a62a817e79bf79c0ce5c6b864ee8ea1bf8adbe
Oguzhan Aslan <aslan@oguzhan.in>
2015-08-18 17:51:34 +0300

    haydi bismillah headeri gonderek ayol

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,6 +14,55 @@ MAIN = {
 
     shareInit: function() {
 
+    },
+
+    menuInit : function(){
+        //move nav element position according to window width
+        moveNavigation();
+        $(window).on('resize', function() {
+            (!window.requestAnimationFrame) ? setTimeout(moveNavigation, 300) : window.requestAnimationFrame(moveNavigation);
+        });
+
+        //mobile version - open/close navigation
+        $('.cd-nav-trigger').on('click', function(event) {
+            event.preventDefault();
+            if ($('header').hasClass('nav-is-visible')) $('.moves-out').removeClass('moves-out');
+
+            $('header').toggleClass('nav-is-visible');
+            $('.cd-main-nav').toggleClass('nav-is-visible');
+            $('.cd-main-content').toggleClass('nav-is-visible');
+        });
+
+        //mobile version - go back to main navigation
+        $('.go-back').on('click', function(event) {
+            event.preventDefault();
+            $('.cd-main-nav').removeClass('moves-out');
+        });
+
+        //open sub-navigation
+        $('.cd-subnav-trigger').on('click', function(event) {
+            event.preventDefault();
+            $('.cd-main-nav').toggleClass('moves-out');
+        });
+
+        function moveNavigation() {
+            var navigation = $('.cd-main-nav-wrapper');
+            var screenSize = checkWindowWidth();
+            if (screenSize) {
+                //desktop screen - insert inside header element
+                navigation.detach();
+                navigation.insertBefore('.cd-nav-trigger');
+            } else {
+                //mobile screen - insert after .cd-main-content element
+                navigation.detach();
+                navigation.insertAfter('.cd-main-content');
+            }
+        }
+
+        function checkWindowWidth() {
+            var mq = window.getComputedStyle(document.querySelector('header'), '::before').getPropertyValue('content').replace(/"/g, '').replace(/'/g, "");
+            return (mq == 'mobile') ? false : true;
+        }
     }
 }
 
@@ -29,4 +78,5 @@ $(function() {
     MAIN.pluginsInit();
     MAIN.mainInit();
     MAIN.shareInit();
+    MAIN.menuInit();
 });
